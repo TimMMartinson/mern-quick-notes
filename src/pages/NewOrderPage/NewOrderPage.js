e8eac6985bfaee5f6a232f4b69d9816a0199686d
timmmartinson <timmmartinson@gmail.com>
2023-02-13 19:12:49 -0700

    submitting what I have

--- src/pages/NewOrderPage/NewOrderPage.js
+++ src/pages/NewOrderPage/NewOrderPage.js
@@ -1,3 +1,8 @@
 export default function NewOrderPage() {
-	return <h2>No Notes Yet</h2>
+	return (
+	<>
+	<h2>No Notes Yet</h2>
+	<p>I've run out of time to complete this homework due to difficulties in cloning down the infrastructure, but I will come back to it.  <br></br>This is what I have for now.</p>
+	</>
+	)
 }
